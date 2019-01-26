d82041deff6db8e7fe717686d857a378d187e454
Gabriel Santiago Cardoso Rizzo <gabriel96.gsr@gmail.com>
2019-01-26 16:59:41 -0200

    Adding transition and button component to details

--- src/domains/bands/components/organisms/presentational/DetailsHolder.vue
+++ src/domains/bands/components/organisms/presentational/DetailsHolder.vue
@@ -6,12 +6,21 @@
     <div class="band-image">
       <img :src="band.src">
     </div>
+    <div class="button-card">
+      <g-card-button class="button"/>
+      <g-card-button class="button"/>
+      <g-card-button class="button"/>
+    </div>
   </div>
 </template>
 
 <script>
+import GCardButton from '@/domains/shared/GCardButton'
 export default {
   name: 'DetailsHolder',
+  components: {
+    GCardButton
+  },
   props: {
     band: {
       type: Object,
@@ -25,7 +34,7 @@ export default {
 .main
   box-sizing border-box
   margin auto
-  width 80%
+  width 90%
 .band-name
   margin-top 3vh
   margin-bottom 3vh
@@ -40,13 +49,24 @@ export default {
   justify-content center
   flex-wrap wrap
   background-color blue
+.button-card
+  margin-top 2vh
+  text-align center
+.button
+  margin-bottom 1vh
+  margin-right 1vw
 .band-image
   margin auto
   max-width 600px
   max-height 550px
   img
+    transition all .2s ease-in-out
     width 100%
     height 100%
     border 2px
     border-radius 15px
+  img:hover
+    margin-bottom 10px
+    transform scale(1.1)
+    opacity 0.8
 </style>
---
+++ src/domains/shared/GCardButton.vue
@@ -0,0 +1,48 @@
+<template>
+  <button
+    type="button"
+    class="button-card-purple"
+    @click="click">
+    <span>{{ text }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'GenericCardButton',
+  props: {
+    text: {
+      type: String,
+      default: 'Click Here For Awesomeness'
+    }
+  },
+  methods: {
+    click ($event) {
+      alert('oi')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.button-card-purple
+  transition all .2s ease-in-out
+  height 10vh
+  cursor pointer
+  text-align center
+  font-family Helvetica
+  font-size 0.6 rem
+  color #b61aff
+  border 1.5px
+  border-style solid
+  border-color #b61aff
+  border-radius 15px
+  background-color #fdfdfd
+.button-card-purple:hover
+  transform scale(0.9)
+  color #570080
+  background-color #e7b3ff
+  opacity 0.6
+span
+  margin 5px
+</style>
